c38bfc385b09c89870e6669edd9df32e81fd2a8a
pamarthi durga rao <pamarthidurgarao1@gmail.com>
2018-05-08 11:55:32 +0530

    folder structure changes

--- src/data.js
+++
@@ -1,9 +0,0 @@
-const data = [
-    { id: 1, user: 'Bryan', text: 'Wow this is neat!', likesCount: 2,commentCount:0},
-    { id: 2, user: 'You', text: 'youre __right!__', likesCount: 2,commentCount:0},
-    { id: 3, user: 'Bryan', text: 'Wow this is neat!', likesCount: 3,commentCount:0},
-    { id: 4, user: 'You', text: 'youre __right!__', likesCount: 0,commentCount:0},
-    { id: 5, user: 'Bryan', text: 'Wow this is neat!', likesCount: 4,commentCount:0},
-    { id: 6, user: 'You', text: 'youre __right!__', likesCount: 1,commentCount:0}
-]
-module.exports = data;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReportTimeline from './ReportTimeline';
-import registerServiceWorker from './registerServiceWorker';
-import './style.css'
-
-ReactDOM.render( <ReportTimeline url="https://jokesbox.herokuapp.com//api/jokes" pollInterval={20000}/> , document.getElementById('root'));
-registerServiceWorker();
